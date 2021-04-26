35abdc6ecbaccb81c68ff7142e65d0c01de4be4d
ngomyn <ngominhhainam@gmail.com>
2021-04-26 22:23:07 +0700

    v4

--- Javascipts/myscripts.js
+++ Javascipts/myscripts.js
@@ -1,11 +1,79 @@
-
 function RunApp() {
+    $('#label-filename').hide();
+    $("#fb-form").validate({
+		rules: {
+			"fullname": {
+				required: true
+			},
+			"phone": {
+				required: true,
+				minlength: 10,
+                maxlength: 12
+			},
+			"selectPosition": {
+				required: true
+			},
+            "exp": {
+				required: true,
+                minlength: 10
+			},
+            "imagefile": {
+                required: true
+            },
+            "email": {
+                required: true,
+                email: true
+            }
+		},
+		messages: {
+			"fullname": {
+				required: "Bắt buộc nhập Họ tên"
+			},
+			"phone": {
+				required: "Bắt buộc nhập số điện thoại",
+				minlength: "Hãy nhập ít nhất 10 số",
+                maxlength: "Hãy nhập ít nhất 12 số"
+			},
+			"selectPosition": {
+                required: "Bắt buộc lựa chọn vị trí ứng tuyển"
+			},
+            "exp": {
+                required: "Bắt buộc nhập kinh nghiệm làm việc",
+                minlength: "Hãy mô tả kinh nghiệm làm việc trên 10 ký tự"
+            },
+            "imagefile": {
+                required: "Bắt buộc đính kèm ảnh"
+            },
+            "email": {
+                required: "Bắt buộc nhập email",
+                email : "Hãy nhập đúng dạng email"
+            }
+		}
+	});
+    $('.form-control, .custom-select, .form-control-file').on('blur', function(){
+        showFileName();
+        if($('#fb-form').valid()) {
+            $('#sub-form').prop('disabled', false);
+        } else {
+            $('#sub-form').prop('disabled', 'disabled');
+        }
+
+    })
+    
     $("form").submit(function(e) { e.preventDefault() });
     $('#fb-form').submit(function () {
         if($(this).valid()) {
             postForm();
         }
     });
+}
+
+function showFileName(){
+    let fileName = $('#exampleFormControlFile3').val().split("\\").slice(-1).pop();
+    if(fileName != null) {
+        $('#label-filename').text(fileName);
+        $('#label-filename').show();
+    }
 }
 function showInfo(message) {
     $('#success').text(message);
@@ -21,13 +89,13 @@ function showError(message) {
 
 function postForm() {
     const urlFreemind = "https://freemind-test.netlify.app/.netlify/functions/test";
-    let image = $('#exampleFormControlFile3').val().split("\\");
+    let image = $('#exampleFormControlFile3').val().split("\\").slice(-1).pop();
     let applyData = {
         name : $('#inputName3').val(),
         phone : $('#inputTel3').val(),
         position: $('#selectPosition').val(),
         exp : $('#FormControlTextarea3').val(),
-        picture : image.slice(-1).pop(),
+        picture : image,
         email : $('#inputEmail3').val()
     }
     $.ajax({
@@ -44,12 +112,7 @@ function postSuccess (response) {
 }
 
 function handleAjaxError(response) {
-    let errorMsg = JSON.stringify(response);
-    if (response.readyState === 0)
-    errorMsg = "Có sự cố về mạng";
-    if (response.responseJSON &&
-    response.responseJSON.description)
-    errorMsg = response.responseJSON.description;
-    showError(errorMsg);
-  }
+
+    showError("Có sự cố về mạng, xin hãy kiểm tra lại.");
+}
 
